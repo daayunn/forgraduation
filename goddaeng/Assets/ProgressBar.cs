43fc3378055f4c529739217b69a4f4c8c66d1f0b
dayun <tlsekdbs0715@naver.com>
2021-01-25 21:37:19 +0900

    b

---
+++ goddaeng/Assets/ProgressBar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode()]
+public class ProgressBar : MonoBehaviour
+{
+    public int max;
+    public int current;
+    public Image mask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        getCurrentFill();
+    }
+    void getCurrentFill()
+    {
+        float fillAmount = (float)current /(float) max;
+        mask.fillAmount = fillAmount;
+    }
+}
